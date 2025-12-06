7ad5ab2752bcfa1975ea43856b1c43354a8f17f4
Sajid <ossajid7756@gmail.com>
2025-12-06 15:06:04 +0600

    removed loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import LoadingScreen from './components/LoadingScreen'
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-gray-900 text-gray-100 font-inter">
-      <LoadingScreen />
-      <Header />
+        <Header />
       <Hero />
       <About />
       <Skills />
@@ -19,7 +17,7 @@ function App() {
       <Contact />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
